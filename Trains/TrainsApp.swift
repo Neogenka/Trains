10845e6c74d0dfc7cf9386b982f65f49827b8e86
m.kozhukhov <neogenka@gmail.com>
2026-01-17 13:44:53 +0300

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by МAK on 17.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
